23e44fcbcdaf99f637f5ed456043a71cfb7d8247
Perttu Auramo <jeah@jeah.com>
2012-11-17 21:04:16 +0200

    sorting, at last

--- lib/content_provider.rb
+++ lib/content_provider.rb
@@ -9,11 +9,12 @@ require 'logger'
 begin
   service = TimetableServiceInitializer.init_service
   time_tables = service.get_time_tables
+  sorted_time_tables = time_tables.sort_by { |bus, nbt| bus.name }
 
   output='<table class="busTable">'
   output +='<tr><th>Bussi</th><th>Suunta</th><th>Min (klo)</th></tr>'
   
-  time_tables.each do |bus, next_bus_times|
+  sorted_time_tables.each do |bus, next_bus_times|
     output += '<tr>'
     output += '<td>' + bus.name + '</td>'
     output += '<td>' + bus.last_stop[0..5] + '</td>'
