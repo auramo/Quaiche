9c26e79361222e4fa417dfc2fd0973c38b13f0c2
Perttu Auramo <perttu.auramo@gmail.com>
2013-12-29 14:03:21 +0200

    Install the front-end bash script also

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ set -e
 
 sudo mkdir -p /usr/local/quaiche 2> /dev/null || true
 sudo cp -r lib/* /usr/local/quaiche/.
+sudo cp run_content_provider.sh /usr/local/quaiche/.
 
 cp -r ui/NextBuses.wdgt ~/Desktop/.
